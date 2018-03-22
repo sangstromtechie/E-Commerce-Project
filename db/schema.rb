32b4f13bb467a6eda7476ff8c3a2460bd6a5942f
Unknown <cwenham@gmail.com>
2018-03-22 14:24:27 -0500

    db:migrate for Orders model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180322191526) do
+ActiveRecord::Schema.define(version: 20180322191824) do
 
   create_table "abouts", force: :cascade do |t|
     t.text "description"
@@ -81,6 +81,15 @@ ActiveRecord::Schema.define(version: 20180322191526) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id"
+    t.string "status"
+    t.float "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
   end
 
   create_table "products", force: :cascade do |t|
