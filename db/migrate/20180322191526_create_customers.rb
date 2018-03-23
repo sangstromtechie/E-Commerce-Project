e8793befbf990531294f0021e965faa3fd642a5d
Unknown <cwenham@gmail.com>
2018-03-23 11:06:05 -0500

    Added Customers to ActiveAdmin

--- db/migrate/20180322191526_create_customers.rb
+++ db/migrate/20180322191526_create_customers.rb
@@ -3,7 +3,6 @@ class CreateCustomers < ActiveRecord::Migration[5.1]
     create_table :customers do |t|
       t.string :name
       t.string :email
-      t.string :phone
       t.string :password
 
       t.timestamps
